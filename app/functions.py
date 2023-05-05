4a4b60ee6a7af1bdc62325e9a4c659bd3ddae411
Fredy ID <56205156+fredy-ID@users.noreply.github.com>
2023-05-05 14:58:24 +0400

    maj fonction predict

--- app/functions.py
+++ app/functions.py
@@ -1,30 +1,29 @@
+import os
+import joblib
+from django.conf import settings
+
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from category_encoders import TargetEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from rest_framework import serializers
+from sklearn.ensemble import RandomForestRegressor
+
+import pandas as pd
 
 from .serializers import *
 
+
+def pipeline_transformer2():
+    model_path = os.path.join(settings.BASE_DIR, 'app', 'models_ai')
+    pipeline_transformer = joblib.load(model_path+'/transformers/full_pipeline2.pkl')
+
+    return pipeline_transformer
+
 def pipeline_transformer():
-    # Features for target encoding
-    car_cat1 = ["Manufacturer", "Model", "Category", "Color"]
-    # Features for one hot encoding
-    car_cat2 = ["Fuel type", "Gear box type", "Drive wheels", "Wheel"]
-
-    #Initialize pipeline
-    num_pipeline = Pipeline([
-        ("imputer", SimpleImputer(strategy='median')),
-        ("std_scalar", StandardScaler())
-    ])
-
-    #full pipeline
-    pipeline_transformer = ColumnTransformer([
-        ("target_encoder", TargetEncoder(), car_cat1),
-        ('num', num_pipeline, ['Levy', 'Prod. year', 'Leather interior', 'Engine volume', 'Mileage', 'Cylinders', 'Doors', 'Airbags', 'Turbo']),
-        ("one_hot_encoder", OneHotEncoder(), car_cat2)
-    ])
+    model_path = os.path.join(settings.BASE_DIR, 'app', 'models_ai')
+    pipeline_transformer = joblib.load(model_path+'/transformers/full_pipeline.pkl')
 
     return pipeline_transformer
 
@@ -34,35 +33,41 @@ def validate_s(serializer):
     else:
         raise serializers.ValidationError(serializer.__str__)
 
-def validate_r(request):
-    levy = request.POST.get('levy')
-    manufacturer = request.POST.get('nom')
-    model = request.POST.get('model')
-    year = request.POST.get('year')
-    category = request.POST.get('category')
-    leather_interior = request.POST.get('leather_interior')
-    fuel_type = request.POST.get('fuel_type')
-    engine_volume = request.POST.get('engine_volume')
-    mileage = request.POST.get('mileage')
-    cylinders = request.POST.get('cylinders')
-    gear_box_type = request.POST.get('gear_box_type')
-    drive_wheels = request.POST.get('drive_wheels')
-    doors = request.POST.get('doors')
-    wheel = request.POST.get('wheel')
-    color = request.POST.get('color')
-    airbags = request.POST.get('airbags')
-    turbo = request.POST.get('turbo')
 
-    validate_s(CarburantsListeSerializer(data=request.data))
-    validate_s(CategoriesListeSerializer(data=request.data))
-    validate_s(CouleursListeSerializer(data=request.data))
-    validate_s(IntervallesProductionListeSerializer(data=request.data))
-    validate_s(MarquesListeSerializer(data=request.data))
-    validate_s(ModelesListeSerializer(data=request.data))
-    validate_s(OuverturesListeSerializer(data=request.data))
-    validate_s(RouesMotricesListeSerializer(data=request.data))
-    validate_s(TransmissionsListeSerializer(data=request.data))
-    validate_s(VoituresListeSerializer(data=request.data))
+def validate_r(request):
+    validate_s(CarburantsListeSerializer(data=request.POST))
+    validate_s(CategoriesListeSerializer(data=request.POST))
+    validate_s(CouleursListeSerializer(data=request.POST))
+    validate_s(IntervallesProductionListeSerializer(data=request.POST))
+    validate_s(MarquesListeSerializer(data=request.POST))
+    # validate_s(ModelesListeSerializer(data=request.POST))
+    validate_s(OuverturesListeSerializer(data=request.POST))
+    validate_s(RouesMotricesListeSerializer(data=request.POST))
+    validate_s(TransmissionsListeSerializer(data=request.POST))
+    # validate_s(VoituresListeSerializer(data=request.POST))
+
+    print(request.data)
+    print(request.data.get('Levy'))
+    print('--------------yyyyy-----------')
+    print(request.POST)
+    
+    levy = request.data.get('Levy')
+    manufacturer = request.data.get('Manufacturer')
+    model = request.data.get('Model')
+    year = request.data.get('Prod. year')
+    category = request.data.get('Category')
+    leather_interior = request.data.get('Leather interior')
+    fuel_type = request.data.get('Fuel type')
+    engine_volume = request.data.get('Engine volume')
+    mileage = request.data.get('Mileage')
+    cylinders = request.data.get('Cylinders')
+    gear_box_type = request.data.get('Gear box type')
+    drive_wheels = request.data.get('Drive wheels')
+    doors = request.data.get('Doors')
+    wheel = request.data.get('Wheel')
+    color = request.data.get('Color')
+    airbags = request.data.get('Airbags')
+    turbo = request.data.get('Turbo')
 
     # initialise data of lists.
     data = {
@@ -85,11 +90,6 @@ def validate_r(request):
         'Turbo':[turbo],
     }
 
-    																
-    
-    # Create DataFrame
     df = pd.DataFrame(data)
 
-    return data
-    
-    
+    return df
--- app/views.py
+++ app/views.py
@@ -1,12 +1,21 @@
+import os
+import pickle
+import joblib
+
+from rest_framework.decorators import api_view
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from django.conf import settings
 
+from .functions import pipeline_transformer, validate_r, pipeline_transformer2
 
-import pickle
-from .functions import pipeline_transformer, validate_r
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.response import Response
+
 
 @login_required(login_url="login")
 def home(request):
@@ -34,15 +43,27 @@ def logout_(request):
 
     return redirect("login")
 
+@csrf_exempt
+@api_view(['POST'])
 def predict(request):
+    model_path = os.path.join(settings.BASE_DIR, 'app', 'models_ai')
+
+    num_model = request.data.get('num_model')
     
-    data = validate_r(request)
-    pipeline = pipeline_transformer()
 
-    predictions = clf2.predict(pipeline.transform(data))
+    if num_model == 2:
+        # with open(model_path+'/model.pkl', 'rb') as f:
+        #     model = pickle.load(f)
+        model = joblib.load(model_path+'/final_model.pkl')
+        df = validate_r(request)
+        pipeline = pipeline_transformer2()
+    else:
+        model = joblib.load(model_path+'/model.pkl')
+        df = validate_r(request)
+        pipeline = pipeline_transformer()
 
-    # load model
-    with open('ai-models/model.pkl', 'rb') as f:
-        clf2 = pickle.load(f)
+
+    predictions = model.predict(pipeline.transform(df))
+    return Response({'result': predictions})
     
 
